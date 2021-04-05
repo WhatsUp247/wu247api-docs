fcf694e31e08fe76ca6da0bf40e76dfb12566dc4
Dominic Cartwright <dominic.cartwright07@gmail.com>
2021-04-05 10:34:13 -0400

    Initial Commit

---
+++ docs/src/.vuepress/config.js
@@ -0,0 +1,69 @@
+const { description } = require("../../package");
+// const path = require("path");
+
+module.exports = {
+	/**
+	 * Ref：https://v1.vuepress.vuejs.org/config/#title
+	 */
+	title: "What'sUp 24/7 API Documentation",
+	/**
+	 * Ref：https://v1.vuepress.vuejs.org/config/#description
+	 */
+	description: description,
+
+	/**
+	 * Extra tags to be injected to the page HTML `<head>`
+	 *
+	 * ref：https://v1.vuepress.vuejs.org/config/#head
+	 */
+	head: [
+		["link", { rel: "icon", href: `https://www.whatsup247.com/img/favicon-16x16.png` }],
+		["link", { rel: "icon", href: `https://www.whatsup247.com/img/favicon-32x32.png` }],
+		["link", { rel: "icon", href: `https://www.whatsup247.com/img/favicon-48x48.png` }],
+		["meta", { name: "theme-color", content: "#418d96" }],
+		["meta", { name: "apple-mobile-web-app-capable", content: "yes" }],
+		["meta", { name: "apple-mobile-web-app-status-bar-style", content: "black" }],
+	],
+
+	/**
+	 * Theme configuration, here is the default theme configuration for VuePress.
+	 *
+	 * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
+	 */
+	themeConfig: {
+		repo: "",
+		logo: "https://www.whatsup247.com/img/wu-logo-icon.svg",
+		editLinks: false,
+		docsDir: "",
+		editLinkText: "",
+		lastUpdated: false,
+		nav: [
+			{
+				text: "Guide",
+				link: "/guide/",
+			},
+			{
+				text: "Config",
+				link: "/config/",
+			},
+			{
+				text: "VuePress",
+				link: "https://v1.vuepress.vuejs.org",
+			},
+		],
+		sidebar: {
+			"/guide/": [
+				{
+					title: "Guide",
+					collapsable: false,
+					children: ["", "using-vue"],
+				},
+			],
+		},
+	},
+
+	/**
+	 * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
+	 */
+	plugins: ["@vuepress/plugin-back-to-top", "@vuepress/plugin-medium-zoom", "vuepress-plugin-versioning"],
+};
---
+++ docs/src/index.md
@@ -0,0 +1,15 @@
+---
+home: true
+heroImage: https://www.whatsup247.com/img/wu-logo.svg
+tagline: Documentation for What'Up 24/7 API
+actionText: Get Started →
+actionLink: /guide/
+# features:
+#     - title: Feature 1 Title
+#       details: Feature 1 Description
+#     - title: Feature 2 Title
+#       details: Feature 2 Description
+#     - title: Feature 3 Title
+#       details: Feature 3 Description
+footer: Made by Dominic Cartwright
+---
